022ed0d7f65765d0126cb7217324c8abd7824724
andiarwidiar1 <andiarkurnia1234@gmail.com>
2023-05-04 23:10:23 +0700

    last perubahan login & register fix

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,14 +1,13 @@
 const urlUserRegister = 'https://64506f04a3221969114a69f9.mockapi.io/user'
 function register() {
   const user = {
-    name: document.getElementById('nameRegister').value,
+    nama: document.getElementById('nameRegister').value,
     email: document.getElementById('emailRegister').value,
     password: document.getElementById('passwordRegister').value,
     password2: document.getElementById('passwordRegister2').value,
     asalsekolah: document.getElementById('asalSekolahRegister').value,
     alasan: document.getElementById('alasanRegister').value,
     alamat: document.getElementById('alamatRegister').value
-
   }
 
   console.log(user)
