c92d2935f0d2ae10324f55657d5d8c4c3d41c9d7
ROHENDI460 <rohendi@upi.edu>
2023-05-06 01:08:36 +0700

    rapihin komentar card

--- assets/js/komen.js
+++ assets/js/komen.js
@@ -1,34 +1,36 @@
-const submit = document.querySelector('.comment-submit');
-const commentList = document.querySelector('.comments');
-const commentInput = document.querySelector('.comment-input');
+const submit = document.querySelector(".comment-submit");
+const commentList = document.querySelector(".comments");
+const commentInput = document.querySelector(".comment-input");
 
 function template(data) {
-  commentList.insertAdjacentHTML("beforeend", `
+  commentList.insertAdjacentHTML(
+    "beforeend",
+    `
   <div class="comment flex items-start justify-start">
       <div class="">
-        <p class="comment-author"><strong>${data.author}</strong> | ${data.comment}</p>
+        <p class="py-3 px-4"><strong>${data.author}</strong> | ${data.comment}</p>
         <br>
       </div>
     </div>
-  </div>`);
+  </div>`
+  );
 }
 
-function appendComment (event) {
-
+function appendComment(event) {
   const data = {
     comment: commentInput.value,
-    author: "Nova Sriady"
+    author: "Nova Sriady",
   };
 
   event.preventDefault();
   if (commentInput.value.length < 1) return;
   template(data);
   commentInput.value = "";
-  localStorage.setItem('commentListing', commentList.innerHTML);
+  localStorage.setItem("commentListing", commentList.innerHTML);
 }
 
-submit.addEventListener('click', appendComment, false)
-const saved = localStorage.getItem('commentListing');
+submit.addEventListener("click", appendComment, false);
+const saved = localStorage.getItem("commentListing");
 if (saved) {
-	commentList.innerHTML = saved;
+  commentList.innerHTML = saved;
 }
